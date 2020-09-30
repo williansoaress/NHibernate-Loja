660b479bf7929eba75820c31f045a55bab3e6a51
Willian Soares <dasilva.williansoares@gmail.com>
2020-09-29 22:17:54 -0300

    Configuração inicial da aplicação

---
+++ NHibernate-Loja/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NHibernate_Loja
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ola");
+            Console.ReadLine();
+        }
+    }
+}
